4d635e673cee5856668f08e80bd43fbd2395e3e1
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-06-12 19:45:28 +0530

    Update __init__.py

--- YuiHirasawaMusicBot/services/converter/__init__.py
+++ YuiHirasawaMusicBot/services/converter/__init__.py
@@ -3,4 +3,4 @@ from os import listdir, mkdir
 if "raw_files" not in listdir():
     mkdir("raw_files")
 
-from DaisyXMusic.services.converter.converter import convert
+from YuiHirasawaMusicBot.services.converter.converter import convert
