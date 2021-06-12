414391d9b9af243d33aa1723cac7d2193f89aba8
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-06-12 20:00:35 +0530

    Update gcast.py

--- YuiHirasawaMusicBot/modules/gcast.py
+++ YuiHirasawaMusicBot/modules/gcast.py
@@ -18,7 +18,7 @@
 from pyrogram import Client, filters
 from pyrogram.errors import UserAlreadyParticipant
 import asyncio
-from DaisyXMusic.config import SUDO_USERS
+from  YuiHirasawaMusicBot.config import SUDO_USERS
 
 @Client.on_message(filters.command(["gcast"]))
 async def bye(client, message):
