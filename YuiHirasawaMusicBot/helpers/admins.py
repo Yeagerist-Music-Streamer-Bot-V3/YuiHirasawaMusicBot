dd6cce49a800399b410ba6efb2bb7bc45c292689
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-06-12 20:07:07 +0530

    Update admins.py

--- YuiHirasawaMusicBot/helpers/admins.py
+++ YuiHirasawaMusicBot/helpers/admins.py
@@ -19,8 +19,8 @@ from typing import List
 
 from pyrogram.types import Chat, User
 
-from DaisyXMusic.function.admins import get as gett
-from DaisyXMusic.function.admins import set
+from YuiHirasawaMusicBot.function.admins import get as gett
+from YuiHirasawaMusicBot.function.admins import set
 
 
 async def get_administrators(chat: Chat) -> List[User]:
