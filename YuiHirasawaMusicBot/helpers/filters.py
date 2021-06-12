b20c314d0c5d6aa541216d1ae0f72cf4cf638ff1
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-06-12 20:08:02 +0530

    Update filters.py

--- YuiHirasawaMusicBot/helpers/filters.py
+++ YuiHirasawaMusicBot/helpers/filters.py
@@ -19,7 +19,7 @@ from typing import List, Union
 
 from pyrogram import filters
 
-from DaisyXMusic.config import COMMAND_PREFIXES
+from YuiHirasawaMusicBot.config import COMMAND_PREFIXES
 
 other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
 other_filters2 = (
