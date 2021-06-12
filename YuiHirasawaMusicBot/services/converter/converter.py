b7f8b48b1ae378a9e6ef4a9bd99a66f819a35860
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-06-12 19:45:57 +0530

    Update converter.py

--- YuiHirasawaMusicBot/services/converter/converter.py
+++ YuiHirasawaMusicBot/services/converter/converter.py
@@ -18,7 +18,7 @@
 import asyncio
 from os import path
 
-from DaisyXMusic.helpers.errors import FFmpegReturnCodeError
+from YuiHirasawaMusicBot.helpers.errors import FFmpegReturnCodeError
 
 
 async def convert(file_path: str) -> str:
