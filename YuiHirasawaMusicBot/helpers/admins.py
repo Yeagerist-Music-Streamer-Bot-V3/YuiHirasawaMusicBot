51f5907d70e52b35bf9a81ed376e439641bdaf37
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-06-27 11:31:26 +0530

    Major Update 5.0
     Major Update 5.0

--- YuiHirasawaMusicBot/helpers/admins.py
+++ YuiHirasawaMusicBot/helpers/admins.py
@@ -1,23 +1,7 @@
-# Calls Music 1 - Telegram bot for streaming audio in group calls
-# Copyright (C) 2021  Roj Serbest
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-
 from typing import List
 
-from pyrogram.types import Chat, User
+from pyrogram.types import Chat
+from pyrogram.types import User
 
 from YuiHirasawaMusicBot.function.admins import get as gett
 from YuiHirasawaMusicBot.function.admins import set
@@ -33,8 +17,8 @@ async def get_administrators(chat: Chat) -> List[User]:
         to_set = []
 
         for administrator in administrators:
-            # if administrator.can_manage_voice_chats:
-            to_set.append(administrator.user.id)
+            if administrator.can_manage_voice_chats:
+                to_set.append(administrator.user.id)
 
         set(chat.id, to_set)
         return await get_administrators(chat)
