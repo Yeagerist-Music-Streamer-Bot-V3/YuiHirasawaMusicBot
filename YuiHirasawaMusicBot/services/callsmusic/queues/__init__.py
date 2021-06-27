140f406d0dd02492fa364cd96b7b3cd0d6d3efdc
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-06-27 11:58:29 +0530

    Delete YuiHirasawaMusicBot/services/callsmusic/queues directory

--- YuiHirasawaMusicBot/services/callsmusic/queues/__init__.py
+++
@@ -1,7 +0,0 @@
-from YuiHirasawaMusicBot.services.queues.queues import clear 
-from YuiHirasawaMusicBot.services.queues.queues import get
-from YuiHirasawaMusicBot.services.queues.queues import is_empty
-from YuiHirasawaMusicBot.services.queues.queues import put
-from YuiHirasawaMusicBot.services.queues.queues import task_done
-
-__all__ = ["clear", "get", "is_empty", "put", "task_done"]
