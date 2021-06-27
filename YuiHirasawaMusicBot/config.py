35cf4df61c591f4e24472dc1ad0e2d9e10645123
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-06-27 10:04:33 +0530

    Major Update 5.0
    Major Update 5.0

--- YuiHirasawaMusicBot/config.py
+++ YuiHirasawaMusicBot/config.py
@@ -1,23 +1,5 @@
-# DAISYXMUSIC- Telegram bot project
-# Copyright (C) 2021  Roj Serbest
-# Copyright (C) 2021  Inuka Asith
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-# Modified by Inukaasith
-
-import os
 from os import getenv
-
+import os
 from dotenv import load_dotenv
 
 if os.path.exists("local.env"):
@@ -28,19 +10,18 @@ SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "Animemusicarchive6")
-BG_IMAGE = getenv("BG_IMAGE", " https://telegra.ph/file/8a4928725cbc36a0f703b.jpg")
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/8a4928725cbc36a0f703b.jpg")
 admins = {}
-API_ID = int(getenv("API_ID"))
+API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH")
 BOT_USERNAME = getenv("BOT_USERNAME")
 ASSISTANT_NAME = getenv("ASSISTANT_NAME", "Rengoku_Kyujoro_Helper")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "Yeageristbots")
-PROJECT_NAME = getenv("PROJECT_NAME", "Yui Hirasawa Music Vr 4")
-SOURCE_CODE = getenv("SOURCE_CODE", "https://github.com/Yeagerist-Music-Streamer-Bot-V3/YuiHirasawaMusicBot")
+PROJECT_NAME = getenv("PROJECT_NAME", "YuiHirasawaMusicBot v5")
+SOURCE_CODE = getenv("SOURCE_CODE", "github.com/Yeagerist-Music-Streamer-Bot-V3/YuiHirasawaMusicBot")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
 ARQ_API_KEY = getenv("ARQ_API_KEY", None)
 PMPERMIT = getenv("PMPERMIT", None)
 LOG_GRP = getenv("LOG_GRP", None)
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
-
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
