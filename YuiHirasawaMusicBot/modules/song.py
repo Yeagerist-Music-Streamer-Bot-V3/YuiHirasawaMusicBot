73a0267aa5bc4d2f79f9e327ad252d3fffd05fc9
Technical_Hunter <85411336+Technical-Hunter@users.noreply.github.com>
2021-07-02 22:28:47 +0530

    Update song.py

--- YuiHirasawaMusicBot/modules/song.py
+++ YuiHirasawaMusicBot/modules/song.py
@@ -36,7 +36,7 @@ def song(client, message):
         query += " " + str(i)
     print(query)
     m = message.reply("🔎 Finding the song...")
-    ydl_opts = {"format": "bestaudio/best"}
+    ydl_opts = {"format": "bestaudi[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
         link = f"https://youtube.com{results[0]['url_suffix']}"
