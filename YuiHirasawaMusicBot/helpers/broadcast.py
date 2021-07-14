24bc817fc473fb59fb399fc8de0210294e111c58
Animesh Verma <85546858+AVBotz-TG@users.noreply.github.com>
2021-07-14 20:10:44 +0530

    Update broadcast.py

--- YuiHirasawaMusicBot/helpers/broadcast.py
+++ YuiHirasawaMusicBot/helpers/broadcast.py
@@ -6,7 +6,7 @@ import asyncio
 import datetime
 import aiofiles
 import traceback
-from configs import Config
+from config import Config
 from YuiHirasawaMusicBot.access_db import db
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
