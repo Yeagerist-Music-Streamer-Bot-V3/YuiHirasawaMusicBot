5a5665686b0ba41e43393aeeee378f61e3cd6159
Technical_Hunter <85411336+Technical-Hunter@users.noreply.github.com>
2021-07-14 21:08:44 +0530

    Update access_db.py

--- YuiHirasawaMusicBot/access_db.py
+++ YuiHirasawaMusicBot/access_db.py
@@ -1,4 +1,4 @@
-from YuiHirasawaMusicBot.config import Config
+from YuiHirasawaMusicBot import Config
 from YuiHirasawaMusicBot.database import Database
 
 db = Database(Config.MONGODB_URI, Config.SESSION_NAME)
