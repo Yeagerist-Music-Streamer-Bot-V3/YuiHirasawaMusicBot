312b992c3104795f40d5b8c88e091cc26dfcd61f
Technical_Hunter <85411336+Technical-Hunter@users.noreply.github.com>
2021-07-14 21:29:41 +0530

    Update add_user.py

--- YuiHirasawaMusicBot/add_user.py
+++ YuiHirasawaMusicBot/add_user.py
@@ -1,4 +1,4 @@
-from YuiHirasawaMusicBot.config import Config
+from YuiHirasawaMusicBot import config
 from YuiHirasawaMusicBot.access_db import db
 from pyrogram import Client
 from pyrogram.types import Message
@@ -7,8 +7,8 @@ from pyrogram.types import Message
 async def AddUserToDatabase(bot: Client, cmd: Message):
     if not await db.is_user_exist(cmd.from_user.id):
         await db.add_user(cmd.from_user.id)
-        if Config.LOG_CHANNEL is not None:
+        if config.LOG_CHANNEL is not None:
             await bot.send_message(
-                int(Config.LOG_CHANNEL),
+                int(config.LOG_CHANNEL),
                 f"**#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{(await bot.get_me()).username} !!**"
             )
